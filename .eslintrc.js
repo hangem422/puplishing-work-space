9c11ca1183000d42e6e236b527803012678144f2
hangem422 <hangem422@gmail.com>
2020-07-23 11:14:54 +0900

    Refactor #1 - change file structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,10 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  rules: { 'global-require': 0 },
+  rules: {
+    'global-require': 0,
+    'no-param-reassign': 0,
+    'no-alert': 0,
+    'func-names': 0,
+  },
 };

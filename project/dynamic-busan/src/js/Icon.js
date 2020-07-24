11ab9bd55dc0f76b246cd6245da56967589f7b29
hangem422 <hangem422@gmail.com>
2020-07-24 15:23:26 +0900

    Function #7 - add 'more-right' icon

--- project/dynamic-busan/src/js/Icon.js
+++ project/dynamic-busan/src/js/Icon.js
@@ -22,3 +22,51 @@ export function dropDownIcon(size = 16, color = '#000000') {
     child: path,
   });
 }
+
+/**
+ * @description More Right SVG Icon을 생성합니다.
+ * @param {number} size Icon 크기
+ * @param {string} color Icon 색상
+ * @returns {HTMLElement} More Right Icon
+ */
+export function moreRight(size = 16, color = '#000000') {
+  const xmlns = 'http://www.w3.org/2000/svg';
+  const xlink = 'http://www.w3.org/1999/xlink';
+
+  const defPath = createElementNS(xmlns, 'path', {
+    id: 'ueq3dnggla',
+    d: 'M0 0H16V16H0z',
+  });
+  const def = createElementNS(xmlns, 'defs', {
+    child: defPath,
+  });
+
+  const use = createElementNS(xmlns, 'use');
+  use.setAttributeNS(xlink, 'xlink:href', '#ueq3dnggla');
+  const mask = createElementNS(xmlns, 'mask', {
+    id: 'qwh3zhnq1b',
+    fill: '#fff',
+    child: use,
+  });
+  const path = createElementNS(xmlns, 'path', {
+    fill: color,
+    'fill-rule': 'nonzero',
+    d:
+      'M4.86 1.232c.391-.327.959-.303 1.322.036l.086.092 5 6c.281.337.307.813.077 1.175l-.077.105-5 6c-.353.424-.984.482-1.408.128-.392-.326-.47-.889-.203-1.307l.075-.101 4.465-5.361L4.732 2.64c-.327-.391-.303-.959.036-1.322l.092-.086z',
+    mask: 'url(#qwh3zhnq1b)',
+  });
+  const g = createElementNS(xmlns, 'g', {
+    fill: 'none',
+    'fill-rule': 'evenodd',
+    child: [mask, path],
+  });
+
+  return createElementNS(xmlns, 'svg', {
+    xmlns,
+    'xmlns:xlink': xlink,
+    width: size.toString(),
+    height: size.toString(),
+    viewBox: '0 0 16 16',
+    child: [def, g],
+  });
+}

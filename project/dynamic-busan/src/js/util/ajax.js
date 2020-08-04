213ee5091e38ba3e0b2baebaae1e70d705edaa0d
hangem422 <hangem422@gmail.com>
2020-08-04 18:01:14 +0900

    Fix #2 - change how api calls are made to fit the modified API spac

--- project/dynamic-busan/src/js/util/ajax.js
+++ project/dynamic-busan/src/js/util/ajax.js
@@ -12,50 +12,61 @@ export function objToQueryURL(obj) {
 
 /**
  * @description Post Ajax 요청 비동기 객체를 반환합니다.
- * @param {string} url Request URL
- * @param {object} data Request Body
- * @param {{ strict: boolean, parse: 'text' | 'json' }} option SecretTextfield의 옵션 값
+ * @typedef {object} option
+ * @property {string} url Request URL
+ * @property {object} data Request Body
+ * @property {object} headers Request Header
+ * @property {boolean} strict HTTP Status가 200이 아니면 에러 발생
+ * @param {option} option Post Ajax의 옵션 값
  * @returns {Promise<any>} Post Ajax 요청 비동기 객체
  */
-export function post(url, data, option = {}) {
-  const strict = option.strict || false;
+export function post(option) {
+  const headers = typeof option.headers === 'object' ? option.headers : {};
+  const data = typeof option.data === 'object' ? option.data : {};
 
-  return fetch(url, {
+  return fetch(option.url, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 'Content-Type': 'application/json', ...headers },
     body: JSON.stringify(data),
   }).then((res) => {
     // strict가 true이면 HTTP Status가 200이 아닐 시 모두 에러처리 합니다.
-    if (strict && res.status !== 200) {
-      throw new Error(`HTTP Status is ${res.status}`);
+    if (option.strict && res.status !== 200) {
+      throw new Error(`HTTP status is ${res.status}`);
     }
 
-    // 타입에따라 응답을 파싱합니다.
-    if (option.parse === 'text') return res.text();
-    return res.json();
+    const contentType = res.headers.get('content-type');
+    if (contentType.startsWith('application/json;')) return res.json();
+    if (contentType.startsWith('text/plain;')) return res.text();
+    throw new Error(`Unsupported response content-type: ${contentType}`);
   });
 }
 
 /**
  * @description Get Ajax 요청 비동기 객체를 반환합니다.
- * @param {string} url Request URL
- * @param {object} data Request Query Parameter Data
- * @param {{ strict: boolean, parse: 'text' | 'json' }} option SecretTextfield의 옵션 값
- * @returns {Promise<any>} Post Ajax 요청 비동기 객체
+ * @typedef {object} option
+ * @property {string} url Request URL
+ * @property {object} data Request Body
+ * @property {object} headers Request Header
+ * @property {boolean} strict HTTP Status가 200이 아니면 에러 발생
+ * @param {option} option Get Ajax의 옵션 값
+ * @returns {Promise<any>} Get Ajax 요청 비동기 객체
  */
-export function get(url, data, option = {}) {
-  const strict = option.strict || false;
+export function get(option) {
+  const headers = typeof option.headers === 'object' ? option.headers : {};
+  const data = typeof option.data === 'object' ? option.data : {};
 
-  return fetch(`${url}?${objToQueryURL(data)}`, {
+  return fetch(`${option.url}?${objToQueryURL(data)}`, {
     method: 'GET',
+    headers,
   }).then((res) => {
     // strict가 true이면 HTTP Status가 200이 아닐 시 모두 에러처리 합니다.
-    if (strict && res.status !== 200) {
+    if (option.strict && res.status !== 200) {
       throw new Error(`HTTP status is ${res.status}`);
     }
 
-    // 타입에따라 응답을 파싱합니다.
-    if (option.parse === 'text') return res.text();
-    return res.json();
+    const contentType = res.headers.get('content-type');
+    if (contentType.startsWith('application/json;')) return res.json();
+    if (contentType.startsWith('text/plain;')) return res.text();
+    throw new Error(`Unsupported response content-type: ${contentType}`);
   });
 }

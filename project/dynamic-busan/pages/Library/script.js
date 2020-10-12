f67825f3fe9c8a851b26d01e103734af6b73d225
hangem422 <hangem422@gmail.com>
2020-10-12 16:20:02 +0900

    Modify - change busan library url

--- project/dynamic-busan/pages/Library/script.js
+++ project/dynamic-busan/pages/Library/script.js
@@ -12,13 +12,13 @@ import './style.css';
 const SEND_VP_API_URL = '/api/v1/request_required_vp';
 const GET_VC_API_URL = '/api/v1/issue_vc';
 const BUSAN_LIBRARY_URL =
-  'https://library.busan.go.kr/busanlibrary/intro/join/index.do?menu_idx=57';
+  'https://library.busan.go.kr/portal/intro/join/index.do?menu_idx=45';
 
 const ERROR_MESSAGE_01 = '유효하지 않은 환경에서 실행할 수 없습니다.';
 const ERROR_MESSAGE_02 = '오류가 발생했습니다. 잠시 후에 다시 시도 해주세요.';
 const ERROR_MESSAGE_03 =
   '도서관 회원증은 부산도서관 회원만 발급받을 수 있습니다';
-const ERROR_MESSAGE_03_SUB = '책이음 서비스 회원가입 바로가기';
+const ERROR_MESSAGE_03_SUB = '부산도서관 회원가입 바로가기';
 
 const ERROR_CASE = ['EBL001', 'EBL002', 'EBL003', 'EBL004', 'EBL005'];
 
